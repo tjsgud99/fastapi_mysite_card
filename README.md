f93e7747c77ecdfeac6c8c1f5d04755491d6ca16
tjsgud99 <lobsterbuger@naver.com>
2024-04-05 10:46:05 +0900

    Add jinja2 template

--- README.md
+++ README.md
@@ -5,3 +5,15 @@ fastapi, jinja2, sqlalchemy, mariadb, docker, docker-compose, aws,  langchain, a
 1.fastapi : 웹 프레임워크 + API
 2.uvicorn : WAS(웹 어플리케이션 서버)
 3.jinja2 : 템플릿 엔진(HTML, CSS, JS)
+
+### Web 프로그래밍 기초 설명
+
+### 1.URL
+  - http://127.0.0.1:8000 = http/localhost:8000
+  - 127.0.0.1과 localhost는 루프백 주소(현재 디바이스의 IP를 의미)
+  - http -> 프로토콜
+  - 8000 -> Port번호
+  - http 프로토콜 제공하는 함수(get, post, put, delete)
+  - http://127.0.0.1:8000/member?id=abc1234&name=cherry -> 쿼리스트링(get 방식)
+  - 숨겨야하는 정보들(post 방식)
+
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 #uvicorn main:app --reload    # was 실행
 import uvicorn
 from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates/")
 
-@app.get("/")
-async def welcome() -> dict:
-    return{
-        "message" : "Hello FastAPI"
-    }
+
+@app.get("/")       ##http://127.0.0.1:8000/
+async def welcome(request: Request) -> dict:                # 컨트롤 c하면 종료
+    return templates.TemplateResponse("index.html", {"request" : request})
