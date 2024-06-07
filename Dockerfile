4737c848b77b45bccd5ae97654990d4d60a5130e
tjsgud99 <lobsterbuger@naver.com>
2024-06-07 10:04:48 +0900

    Update files

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,10 @@ WORKDIR /app
 
 COPY requirements.txt /app/requirements.txt
 
-RUN pip install --upgrade && pip install -r /app/requirements.txt
+RUN pip install --upgrade pip && pip install -r /app/requirements.txt
 
 EXPOSE 8000
 
-COPY ./ /app/
+COPY ./ /app
 
 CMD [ "python", "main.py" ]
--- main.py
+++ main.py
@@ -1,31 +1,35 @@
-#uvicorn main:app --reload    # was 실행
+# uvicorn main:app --reload    # was 실행
+
 import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
+from fastapi.staticfiles import StaticFiles 
 from backend.routes import kakao, chat
 from apscheduler.schedulers.background import BackgroundScheduler
 from backend.services.service_kakao import KakaoService
+from dotenv import load_dotenv, find_dotenv
+
+_ = load_dotenv(find_dotenv())
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates/")
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory="static"), name="static") 
 
 app.include_router(kakao.router, prefix="/kakao")
 app.include_router(chat.router, prefix="/chat")
 
-@app.get("/")       ##http://127.0.0.1:8000/
-async def welcome(request: Request) :                # 컨트롤 c하면 종료
-    return templates.TemplateResponse("index.html", {"request" : request})
+
+@app.get("/")  # http://127.0.0.1:8000/
+async def welcome(request: Request) -> dict:
+    return templates.TemplateResponse("index.html", {"request": request})
+
 
 if __name__ == "__main__":
-    # 스케줄러
-    # -카카오톡 Refresh token을 재발급 받는 기능
-    # -매월 1일 동작
     sched = BackgroundScheduler()
-    sched.add_job(KakaoService().refresh_access_token, "cron", day="1", hour="0", id="refresh_token")
+    sched.add_job(KakaoService().refresh_access_token, "cron", day="1", hour="0", id="refresh_token")  # 매월 1일 refresh_token 재발급
     sched.start()
+    
+    # uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    
 
-    # 웹서버 동작!
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
 
